f6aab9f822778592aeacf88c3b58aaf5c5d35778
Akemi <akemikoba1903@gmail.com>
2024-06-15 15:00:04 -0300

    first project

---
+++ intro-to-vue-3/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            color: 'blue',
+            description: 'These blue socks are very comfortable and cheap, buy now!'
+        }
+    }
+})
