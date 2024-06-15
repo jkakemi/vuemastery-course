c714cb96ff67195bdaa3362f7fbbe06068153921
Akemi <akemikoba1903@gmail.com>
2024-06-15 15:34:38 -0300

    commited

--- intro-to-vue-3/main.js
+++ intro-to-vue-3/main.js
@@ -1,22 +1,36 @@
 // AULA 1
-// const app = Vue.createApp({
-//     data(){
-//         return {
-//             product: 'Socks',
-//             color: 'blue',
-//             description: 'These blue socks are very comfortable and cheap, buy now!'
-//         }
-//     }
-// })
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Socks',
+            color: 'blue',
+            description: 'These blue socks are very comfortable and cheap, buy now!'
+        }
+    }
+})
 
 // AULA 2
 
-const app = Vue.createApp({
+const app1 = Vue.createApp({
     data(){
         return{
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.amazon.com.br/socks/s?k=socks'
+        }
+    }
+})
+
+// AULA 3
+
+const app2 = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            //inStock: true
+            inventory: 0,
+            onSale: true
         }
     }
 })
