a97508b939d7b045099b013760de2257f23735ae
Akemi <akemikoba1903@gmail.com>
2024-06-16 12:11:37 -0300

    commited

--- intro-to-vue-3/main.js
+++ intro-to-vue-3/main.js
@@ -54,3 +54,39 @@ const app3 = Vue.createApp({
         }
     }
 })
+
+// AULA 5
+
+const app4 = Vue.createApp({
+    data(){
+        return{
+            cart: 0,
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            inStock: true,
+            // inventory: 100,
+            // Lista para percorrer
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+            ],
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        removeToCart(){
+            if(this.cart < 1){
+                this.cart = 0;
+            }
+            else{
+                this.cart -= 1
+            }
+        }
+    }
+})
